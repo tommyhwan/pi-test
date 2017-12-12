89b273ac3197c21945da9d479a1d8ac6c044cc9c
TommyWan <tommy.wan@clarius.me>
2017-12-11 16:43:50 -0800

    update #13

--- stage4/00-install-packages/03-run.sh
+++ stage4/00-install-packages/03-run.sh
@@ -1,19 +1,29 @@
 #!/bin/bash -e
 
 on_chroot << EOF
-wget -qO - https://http://mirrordirector.raspbian.org/raspbian.public.key sudo apt-key add -
+wget -qO - https://mirrordirector.raspbian.org/raspbian.public.key | sudo apt-key add -
 add-apt-repository 'deb http://mirrordirector.raspbian.org/raspbian/ stretch main contrib non-free rpi'
 add-apt-repository 'deb-src http://archive.raspbian.org/raspbian/ stretch main contrib non-free rpi'
-add-apt-repository 'deb-src http://archive.raspbian.org/raspbian/ buster main contrib non-free rpi'
-
+add-apt-repository 'deb http://archive.raspbian.org/raspbian/ buster main contrib non-free rpi'
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y -f libglib2.0-dev
-add-apt-repository -r 'deb-src http://archive.raspbian.org/raspbian/ buster main contrib non-free rpi'
+apt-get install -y -f openssl=1.0.2 libglib2.0-dev
+add-apt-repository -r 'deb http://archive.raspbian.org/raspbian/ buster main contrib non-free rpi'
 EOF
 
 # on_chroot << EOF
 # add-apt-repository 'deb http://ftp.debian.org/debian jessie-backports main'
+# apt-get update -y
+# apt-get upgrade -y
+# apt-get remove -y openssl
+# apt-get install -y -f openssl=1.0.2 libssl=1.0.0
+# add-apt-repository -r 'deb http://ftp.debian.org/debian jessie-backports main'
+# EOF
 #
-#
+# on_chroot << EOF
+# add-apt-repository 'deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi'
+# apt-get update -y
+# apt-get upgrade -y
+# apt-get install -y -f qt4-xll libqt5gui5
+# dd-apt-repository -f 'deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi'
 # EOF
